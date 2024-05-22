5e9eba70fd311f27a23be59a6969d52d4bd1b5d4
anastasiaBliznetsova <bliznecovanastasia@gmail.com>
2024-05-22 23:27:03 +0200

    JDBC

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
+import hexlet.code.repository.BaseRepository;
+
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) {
@@ -13,7 +20,17 @@ public class App {
         String port = System.getenv().getOrDefault("PORT", "8090");
         return Integer.valueOf(port);
     }
-    public static Javalin getApp() {
+    public static Javalin getApp()  throws IOException, SQLException {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:project");
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        var sql = "CREATE TABLE urls (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), createdAt TIMESTAMP NOT NULL)";
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
