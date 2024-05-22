8be1a888081594a18a99612988a5c68dd37f9cea
anastasiaBliznetsova <bliznecovanastasia@gmail.com>
2024-05-22 23:46:37 +0200

    JDBC2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,13 +5,10 @@ import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import hexlet.code.repository.BaseRepository;
-
-import java.io.IOException;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         var app = getApp();
         app.start(getPort());
     }
@@ -20,11 +17,12 @@ public class App {
         String port = System.getenv().getOrDefault("PORT", "8090");
         return Integer.valueOf(port);
     }
-    public static Javalin getApp()  throws IOException, SQLException {
+    public static Javalin getApp()  throws SQLException {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl("jdbc:h2:mem:project");
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-        var sql = "CREATE TABLE urls (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), createdAt TIMESTAMP NOT NULL)";
+        var sql = "CREATE TABLE urls (id BIGINT PRIMARY KEY AUTO_INCREMENT,"
+                + " name VARCHAR(255), createdAt TIMESTAMP NOT NULL)";
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
             statement.execute(sql);
